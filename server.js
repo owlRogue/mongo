f45c91343c8e84cc13ccdabede02fc0dbe2a2629
Koltyn Palmer <koltynchiaro@gmail.com>
2018-06-11 03:11:28 +0200

    take 392434232

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(express.static("public"));
 // mongoose.connect("mongodb://localhost/article_db");
 
 // If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/article_db";
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
 // Connect to the Mongo DB
